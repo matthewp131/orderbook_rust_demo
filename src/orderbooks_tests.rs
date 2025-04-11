655b8bb5b9ec727813c25690c4ff91bbd74525b7
matthewp131 <13476383+matthewp131@users.noreply.github.com>
2025-04-11 09:27:47 +0800

    added scenario3

--- src/orderbooks_tests.rs
+++ src/orderbooks_tests.rs
@@ -43,4 +43,35 @@ mod tests {
         assert_eq!(order_results[11], OrderResult::Acknowledgement { user: 2, user_order_id: 104 });
         assert_eq!(order_results[12], OrderResult::TopOfBookChange { side: 'S', price: "11".to_string(), total_quantity: "200".to_string() });
     }
+
+    #[test]
+    fn scenario3() {
+        let mut order_books = OrderBooks::new(false);
+
+        let orders: Vec<Box<dyn Any>> = vec![
+            Box::new(NewOrder::new(1, "VAL".to_string(), 10, 100, 'B', 1)),
+            Box::new(NewOrder::new(2, "VAL".to_string(), 9, 100, 'B', 101)),
+            Box::new(NewOrder::new(2, "VAL".to_string(), 11, 100, 'S', 102)),
+            Box::new(NewOrder::new(1, "VAL".to_string(), 11, 100, 'B', 2)),
+            Box::new(NewOrder::new(2, "VAL".to_string(), 11, 100, 'S', 103)),
+        ];
+
+        let mut order_results: Vec<OrderResult> = vec![];
+        for order in orders {
+            if let Some(new_order) = order.downcast_ref::<NewOrder>() {
+                order_results.append(&mut order_books.add_order(new_order.clone()))
+            } else if let Some(cancel_order) = order.downcast_ref::<CancelOrder>() {
+                order_results.append(&mut order_books.cancel_order(cancel_order.clone()))
+            }
+        }
+
+        assert_eq!(order_results[0], OrderResult::Acknowledgement { user: 1, user_order_id: 1 });
+        assert_eq!(order_results[1], OrderResult::TopOfBookChange { side: 'B', price: "10".to_string(), total_quantity: "100".to_string() });
+        assert_eq!(order_results[2], OrderResult::Acknowledgement { user: 2, user_order_id: 101 });
+        assert_eq!(order_results[3], OrderResult::Acknowledgement { user: 2, user_order_id: 102 });
+        assert_eq!(order_results[4], OrderResult::TopOfBookChange { side: 'S', price: "11".to_string(), total_quantity: "100".to_string() });
+        assert_eq!(order_results[5], OrderResult::Rejection { user: 1, user_order_id: 2 });
+        assert_eq!(order_results[6], OrderResult::Acknowledgement { user: 2, user_order_id: 103 });
+        assert_eq!(order_results[7], OrderResult::TopOfBookChange { side: 'S', price: "11".to_string(), total_quantity: "200".to_string() });
+    }
 }
