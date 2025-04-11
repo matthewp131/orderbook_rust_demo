4f764185139ff64bc0fe2bbeb087dcd56671a632
matthewp131 <13476383+matthewp131@users.noreply.github.com>
2025-04-11 09:27:46 +0800

    setup initial two threads with message passing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::thread;
+use std::sync::{mpsc, Arc};
+use std::sync::Mutex;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+    
+    let input_thread = thread::spawn(move || {
+        for _ in 0..10 {
+            tx.send("hello").unwrap();
+        }
+    });
+
+    let output_thread = thread::spawn(move || {
+        for x in rx {
+            println!("{x}");
+        }
+    });
+
+    input_thread.join().unwrap();
+    output_thread.join().unwrap();
 }
